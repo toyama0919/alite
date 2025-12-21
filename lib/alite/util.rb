ca339a92bd1ba518958fb076e92190f39d6aa125
Hiroshi Toyama <toyama0919@gmail.com>
2025-12-21 21:16:08 +0900

    update.

--- lib/alite/util.rb
+++ lib/alite/util.rb
@@ -6,6 +6,8 @@ require 'erb'
 module Alite
   class Util
     def self.get_profile(config_path, profile)
+      return nil unless File.exist?(config_path)
+
       config = YAML.safe_load(ERB.new(File.read(config_path)).result(binding), permitted_classes: [Symbol],
                                                                                aliases: true)
       config[profile]
